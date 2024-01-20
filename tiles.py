94bf27ca10489b3e4ae42981ee93ea1cd32b65ad
NESO12 <dadsa.2001@mail.ru>
2024-01-20 20:36:02 +0300

    animations

--- tiles.py
+++ tiles.py
@@ -25,4 +25,22 @@ class StaticTile(Tile):
 class AnimatedTile(Tile):
     def __init__(self, size, x, y, path):
         super().__init__(size, x, y)
-        self.frames = import_folder('levels')
+        self.frames = import_folder('\sprites\score')
+
+        def animate(self):
+            self.frame_index += 0.15
+            if self.frame_index >= len(self.frames):
+                self.frame_index = 0
+            self.imag = self.frames[int(self.frame_index)]
+
+        def update(self, shift):
+            self.animate()
+            self.rect.x += shift
+
+
+class Coin(AnimatedTile):
+    def __init__(self, size, x, y, path='\sprites\score'):
+        super().__init__(size, x, y, path)
+        center_x = x + int(size / 2)
+        center_y = y + int(size / 2)
+        self.rect = self.image.get_rect(center=(center_x, center_y))
